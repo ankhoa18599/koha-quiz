0553bf5e66e002f806e90d6c20ea368da3ca5200
Khoa Tran <ankhoa18599@gmail.com>
2025-03-02 21:27:16 +0700

    Sets up project scaffolding
    Adds essential configuration files for linting, formatting, and IDE settings.
    Includes a comprehensive `.gitignore` to manage project files.
    Configures Next.js, Tailwind CSS, and TypeScript for development.

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="container text-headline-1 flex h-screen items-center justify-center gap-10 bg-slate-400 font-bold text-white">
+      <div className="flex gap-10">hello</div>
+    </div>
+  );
+}
---
+++ src/constants/tailwind.ts
@@ -0,0 +1,61 @@
+export const TYPOGRAPHY = {
+  "title-1": {
+    fontSize: "40px",
+    lineHeight: "56px",
+    responsive: {
+      lg: {
+        fontSize: "64px",
+        lineHeight: "84px",
+      },
+    },
+  },
+};
+export const COLOR = {
+  dark: "#333333",
+};
+export const BG_COLOR = {
+  // slate: {
+  //   900: "#6a6967",
+  //   800: "#8b8a87",
+  //   700: "#b4b2ae",
+  //   600: "#e6e4df",
+  //   500: "#fdfbf5",
+  //   400: "#fdfcf7",
+  //   300: "#fefcf8",
+  //   200: "#fefdfa",
+  //   100: "#fefefc",
+  //   50: "#fffffe",
+  // },
+};
+export const WIDTH = {
+  sm: "336px",
+  md: "448px",
+  lg: "576px",
+  xl: "768px",
+  "2xl": "960px",
+  "3xl": "1056px",
+};
+export const PADDING = {
+  15: "3.75rem",
+};
+export const SPACING = {
+  7.5: "1.875rem",
+  15: "3.75rem",
+};
+export const CONTAINER_WIDTH = {
+  sm: "540px",
+  md: "720px",
+  lg: "960px",
+  xl: "1140px",
+};
+export const BOX_SHADOW = {
+  sm: "0px 1px 2px 0px #00000014",
+  md: "0px 4px 6px -1px #0000001A, 0px 2px 4px -2px #00000080",
+  lg: "0px 10px 15px -3px #0000001A, 0px 4px 6px 0px #0000000D",
+  xl: "0px 20px 25px -5px #0000001A, 0px 10px 10px 0px #0000000A",
+  "2xl": "0px 25px 50px -12px #00000040",
+};
+export const FONT = {
+  manrope: ["var(--font-manrope)"],
+  "dm-serif-text": ["var(--font-dm-serif-text)"],
+};
